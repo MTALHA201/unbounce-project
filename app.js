ef1ab7fdf481b7d2120024bc0b7bcd81ef56418b
m.talha <talhapositive201@gmail.com>
2023-05-11 17:15:04 +0500

    some changing

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll',(e)=>{
+    const nav = document.querySelector('nav');
+    if(window.pageYOffset>0){
+      nav.classList.add("shadow");
+    }else{
+      nav.classList.remove("shadow");
+    }
+  });
+  
